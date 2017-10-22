7de876ccdd2edd374e8d21a20362b8dbfc6f7512
Abhijeet <abhijeetrastogi1997@gmail.com>
2017-10-23 00:29:08 +0530

    Ghost added

---
+++ ghost/app/src/main/java/com/google/engedu/ghost/SimpleDictionary.java
@@ -0,0 +1,77 @@
+/* Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.engedu.ghost;
+
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public class SimpleDictionary implements GhostDictionary {
+    public ArrayList<String> words;
+
+    public SimpleDictionary(InputStream wordListStream) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(wordListStream));
+        words = new ArrayList<>();
+        String line = null;
+        while((line = in.readLine()) != null) {
+            String word = line.trim();
+            if (word.length() >= MIN_WORD_LENGTH)
+              words.add(line.trim());
+        }
+    }
+
+    @Override
+    public boolean isWord(String word) {
+        return words.contains(word);
+    }
+
+    @Override
+    public String getAnyWordStartingWith(String prefix) {
+        if(prefix == null) {
+            return words.get(new Random().nextInt(words.size()));
+        }else {
+            return binarySearch(prefix, 0, words.size() - 1);
+        }
+    }
+
+    public String binarySearch(String prefix, int start, int last){
+        if(start>last){
+            return null;
+        }else{
+            int mid = (start+last)/2;
+            if(words.get(mid).contains(prefix)){
+                return words.get(mid);
+            }else if(words.get(mid).charAt(0)<= prefix.charAt(0)){
+                binarySearch(prefix, start, mid-1);
+            }else{
+                binarySearch(prefix, mid+1, last);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String getGoodWordStartingWith(String prefix) {
+        String selected = null;
+        return selected;
+    }
+}
